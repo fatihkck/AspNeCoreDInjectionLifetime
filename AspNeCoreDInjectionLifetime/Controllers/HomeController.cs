9e1dff559d4aaf68f52efe18d33b7b1f8d7d2159
Fatih <Fatih@DESKTOP-A5H67EF>
2018-12-19 22:21:41 +0300

    genesis

--- AspNeCoreDInjectionLifetime/Controllers/HomeController.cs
+++ AspNeCoreDInjectionLifetime/Controllers/HomeController.cs
@@ -1,18 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNeCoreDInjectionLifetime.Models;
+using AspNeCoreDInjectionLifetime.Services;
 using Microsoft.AspNetCore.Mvc;
-using AspNeCoreDInjectionLifetime.Models;
+using System.Diagnostics;
+using System.IO;
 
 namespace AspNeCoreDInjectionLifetime.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        
+        private readonly OperationService OperationService;
+        private readonly IOperationTransient TransientOperation;
+        private readonly IOperationScoped ScopedOperation;
+        private readonly IOperationSingleton SingletonOperation;
+        private readonly IOperationSingletonInstance SingletonInstanceOperation;
+
+        public HomeController(
+            OperationService operationService,
+        IOperationTransient transientOperation,
+        IOperationScoped scopedOperation,
+        IOperationSingleton singletonOperation,
+        IOperationSingletonInstance singletonInstanceOperation)
         {
-            return View();
+            
+            OperationService = operationService;
+            TransientOperation = transientOperation;
+            ScopedOperation = scopedOperation;
+            SingletonOperation = singletonOperation;
+            SingletonInstanceOperation = singletonInstanceOperation;
+        }
+
+        public void Index()
+        {
+            string transient = $"Transient :  {TransientOperation.OperationId}";
+            string scoped = $"Scoped :  {ScopedOperation.OperationId}";
+            string singleton = $"Singleton :  {SingletonOperation.OperationId}";
+            string SingletonInstance = $"Singleton Instance :  {SingletonInstanceOperation.OperationId}";
+
+
+            string transientOp = $"Transient (always different)  :  {OperationService.TransientOperation.OperationId}";
+            string scopedOp = $"Scoped (same as top within a request but different across requests) :  {OperationService.ScopedOperation.OperationId}";
+            string singletonOp = $"Singleton(always same) :  {OperationService.SingletonOperation.OperationId}";
+            string SingletonInstanceOp = $"Singleton Instance :  {OperationService.SingletonInstanceOperation.OperationId}";
+
+            using (StreamWriter wr = new StreamWriter(HttpContext.Response.Body))
+            {
+                wr.WriteLine("Controller Service");
+                wr.WriteLine(transient);
+                wr.WriteLine(scoped);
+                wr.WriteLine(singleton);
+                wr.WriteLine(SingletonInstance);
+                wr.WriteLine("");
+                wr.WriteLine("");
+                wr.WriteLine("OperationService Operations");
+                wr.WriteLine(transientOp);
+                wr.WriteLine(scopedOp);
+                wr.WriteLine(singletonOp);
+                wr.WriteLine(SingletonInstanceOp);
+
+                wr.Flush();
+
+            }
         }
 
         public IActionResult About()
--- AspNeCoreDInjectionLifetime/Startup.cs
+++ AspNeCoreDInjectionLifetime/Startup.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNeCoreDInjectionLifetime.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+
 
 namespace AspNeCoreDInjectionLifetime
 {
@@ -32,6 +31,15 @@ namespace AspNeCoreDInjectionLifetime
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            services.AddTransient<IOperationTransient, Operation>();
+            services.AddScoped<IOperationScoped, Operation>();
+            services.AddSingleton<IOperationSingleton, Operation>();
+            services.AddSingleton<IOperationSingletonInstance>(new Operation(Guid.Empty));
+
+            // OperationService depends on each of the other Operation types.
+            services.AddTransient<OperationService, OperationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
